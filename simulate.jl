35b4eefc01e44ac9cd96ff896a8d02a58ddf3525
david <david@goldbell.org>
2021-10-25 12:12:02 -0700

    first run of simulate.jl.  Ran all the way through, but I haven't checked the output.

--- simulate.jl
+++ simulate.jl
@@ -22,14 +22,9 @@ end
 mutable struct Process
   parent::Int64
   generation::Int64
-  childen::Array{Int64}
+  has_children::Bool
   start_time::Float64
 end
-
-function has_children(process::Process)::Bool
-  return length(process.children) > 0
-end
-
 
 function main(cmd_line = ARGS)    
   defaults = Dict{String,Any}(
@@ -48,7 +43,7 @@ function main(cmd_line = ARGS)
   total_time = defaults["total_time"]
   k_hat_base = defaults["k_hat_base"]
   k_hat_child = defaults["k_hat_child"]
-  child_half_file = defaults["child_half_life"]
+  child_half_life = defaults["child_half_life"]
 
   data = HawkesPoint[]
   processes = Process[]
@@ -59,35 +54,38 @@ function main(cmd_line = ARGS)
   i = 0
   lambda_0 = k_hat_base/total_time;
   
-  parent_process = Process(-1,-1,[],0) # dummy parent data for base process
+  parent_process = Process(-1,-1,false,0) # dummy parent data for base process
   parent_point = HawkesPoint(-1,0)
   rho = 0
   sigma = k_hat_base/total_time
-  
+  println("dummy parent for base process: $parent_process")
   while next_parent <= length(data)
+    # global parent_process, parent_point, rho, sigma
     if(next_parent > 0)
       parent_point = data[next_parent]
       parent_process = processes[parent_point.process]
-      if has_children(parent_process)
+      if parent_process.has_children
         next_parent += 1
         continue
-        rho = log(2)/child_half_life # decay rate of child lambda
-        sigma = rho*k_hat_child/(1 - exp(-rho*(total_time - parent_point.time)))
       end
+      rho = log(2)/child_half_life # decay rate of child lambda
+      sigma = rho*k_hat_child/(1 - exp(-rho*(total_time - parent_point.time)))
     end
     println("parent process: $parent_process")
-    # OK, we have a new parent. Set up the child process
-    this_process = Process(next_parent,parent_process.generation+1,[],parent_point.time)
+     # OK, we have a new parent. Set up the child process
+    this_process = Process(next_parent,parent_process.generation+1,false,parent_point.time)
     push!(processes,this_process) # save the new process
-    push!(parent_process.children,length(processes)) # add the index of the child process to the parent process  
-
+    this_process_index = length(processes)
+    println(fieldnames(Process))
+    parent_process.has_children = true
     println("this process: $this_process")
+    t0 = this_process.start_time
+    t = t0
     while true # generate the next point
-      lambda = 2^(-this_process.generation)*sigma*exp(-rho*(t - this_process.start_time))
-      t0 -= log(1-rand(rng))/lambda;
-      if t0 > total_time;break;end
-      t = t0
-      push!(data,HawkesPoint(this_process,t))
+      lambda = sigma*exp(-rho*(t - t0))/(2^(this_process.generation))
+      t -= log(1-rand(rng))/lambda;
+      if t > total_time;break;end
+      push!(data,HawkesPoint(this_process_index,t))
       println(data[length(data)])
     end # while true
     next_parent += 1
