ed21d636e84982658f84cfcbf923febb93448c69
david <david@goldbell.org>
2021-10-26 16:09:45 -0700

    simulate looks like it's working.  Next up: hawkes.jl

--- simulate.jl
+++ simulate.jl
@@ -1,8 +1,8 @@
-Things to check with default parameters:
-1.  the generation is 1-up, probably wrong
-2.  the parent field should be an index into processes, not into data
-
 #!/home/david/julia-1.6.1/bin/julia
+# Things to check with default parameters:
+# 1.  the generation is 1-up, probably wrong
+# 2.  the parent field should be an index into processes, not into data
+
 #import GZip
 if !@isdefined(CommandLine_loaded)
   include("CommandLine.jl")
@@ -31,10 +31,10 @@ function Base.:<(x::HawkesPoint,y::HawkesPoint)
 end
 
 mutable struct Process
-  index::Int64
+  process_no::Int64
   parent::Int64
   generation::Int64
-  has_children::Bool
+#  has_children::Bool
   start_time::Float64
 end
 
@@ -44,8 +44,8 @@ function main(cmd_line = ARGS)
     "file_dir" => "",
     "out_file"=>"hawkes_test_data.txt",
     "total_time" => 10,
-    "k_hat_base" => 10, #expected total no. of base process events, so base rate is 10/10 = 1
-    "k_hat_child" => 2, # expected total number of child process events
+    "lambda_0" => 1, # base process rate
+    "sigma_0" => 2, # initial child process rate (see below)
     "child_half_life" => .1, #half-life of child processes 
   )
   cl = get_vals(defaults,cmd_line) # replace defaults with command line values if they are specified
@@ -53,62 +53,54 @@ function main(cmd_line = ARGS)
   seed = defaults["seed"]
   out_file = defaults["out_file"]
   total_time = defaults["total_time"]
-  k_hat_base = defaults["k_hat_base"]
-  k_hat_child = defaults["k_hat_child"]
+  lambda_0 = defaults["lambda_0"]
+  sigma_0 = defaults["sigma_0"]
   child_half_life = defaults["child_half_life"]
 
   data = HawkesPoint[]
   processes = Process[]
   next_process::Int64 = 0 #indexes the processes array
-  next_parent::Int64 = 0 #indexes the data array
+  next_index::Int64 = 0 #indexes the data array
   rng = MersenneTwister(seed)
   t = 0
   i = 0
-  lambda_0 = k_hat_base/total_time;
   
-  parent_process = Process(-1,-1,-1,false,0) # dummy parent data for base process
-  parent_point = HawkesPoint(-1,0)
-  rho = 0
-  sigma = k_hat_base/total_time
+  parent_process = Process(0,-1,-1,0) # dummy parent data for base process
+  data_point = HawkesPoint(0,0)
+  rho = 0 #fake values for base process
+  sigma = lambda_0
   println("dummy parent for base process: $parent_process")
-  while next_parent <= length(data)
-    if(next_parent > 0)
-      println("next_parent: data[$(next_parent)] = $(data[next_parent])") 
-      parent_point = data[next_parent]
-      parent_process = processes[parent_point.process]
-      if parent_process.has_children
-        next_parent += 1
-        continue
-      end
+  
+  while next_index <= length(data)
+    if(next_index > 0) # skip the startup for the base process
+ #     println("next point: data[$(next_index)] = $(data[next_index])") 
+      data_point = data[next_index]
+      parent_process = processes[data_point.process]
       rho = log(2)/child_half_life # decay rate of child lambda
-      sigma = rho*k_hat_child/(1 - exp(-rho*(total_time - parent_point.time)))
+      sigma = sigma_0
     end
-    println("parent process: $parent_process")
-     # OK, we have a new parent. Set up the child process
-    this_process = Process(length(processes)+1,next_parent,parent_process.generation+1,false,parent_point.time)
-    push!(processes,this_process) # save the new process
-#    println(fieldnames(Process))
-    parent_process.has_children = true
- #   println("this process: $this_process")
-    t0 = this_process.start_time
+     # OK, we have a new parent. Set up the child process (this will be the base on the first time thru
+    child_process = Process(length(processes)+1,parent_process.process_no,parent_process.generation+1,data_point.time)
+    push!(processes,child_process) # save the child process
+    t0 = child_process.start_time
     t = t0
     while true # generate the next point
-      lambda = sigma*exp(-rho*(t - t0))/(2^(this_process.generation))
-      t -= log(1-rand(rng))/lambda;
+      lambda = sigma*exp(-rho*(t-t0))/(2^(child_process.generation))
+      y = rand(rng)
+      t -= log(1-y)/lambda;
       if t > total_time;break;end
-      push!(data,HawkesPoint(this_process.index,t))
-#      println(data[length(data)])
+      push!(data,HawkesPoint(child_process.process_no,t))
     end # while true
-    next_parent += 1
-  end # while(next_parent <= length(data)
+    next_index += 1
+  end # while(next_index <= length(data)
   heapsort(data)
+  println("data:\n")
   for point in data
     println("process: $(point.process) time: $(point.time)")
   end
   println("processes:\n")
   for p in processes
-    println("$(p.index): parent: $(p.parent) generation: $(p.generation) has_children? $(p.has_children) start_time: 
-$(p.start_time)")
+    println("$(p.process_no): parent: $(p.parent) generation: $(p.generation)  start_time: $(p.start_time)")
     end
 end #main
 
