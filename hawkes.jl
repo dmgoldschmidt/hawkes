8744a52f80b6a6c863000576e57eaf14799733b6
david <david@goldbell.org>
2021-10-30 10:35:44 -0700

    WIP hawkes.jl

--- hawkes.jl
+++ hawkes.jl
@@ -17,8 +17,9 @@ using Random
 mutable struct Parameters
   lambda::Float64
   omega::Array{Float64}
-  rho::Array{Float64}
-  sigma::Array{Float64}
+  decay_params::Dict{String,Vector{Float64}}
+  #  rho::Array{Float64}
+#  sigma::Array{Float64}
 end
 
 mutable struct HawkesPoint
@@ -59,65 +60,59 @@ function main(cmd_line = ARGS)
   rho_0 = defaults["rho_0"]
   sigma_0 = defaults["sigma_0"]
   lambda_0 = defaults["lambda_0"]
+
+  #Now read the data and initialize the parameters
   
   stream = tryopen(in_file) # this is from util.jl
   all_lines = readlines(stream)
   if(ndata == 0)
     ndata = length(all_lines)
   end
-  decay_params = Dict{String,Vector{Float64}}()
   nmarks = 0
   data = HawkesPoint[]
   println("$ndata data points:\n") 
+  p0 = Parameters(lambda_0,[],Dict())
   for i in 1:ndata
     field = map(string,split(all_lines[i])) # split the ith line
-    if !haskey(decay_params,field[1])
+    if !haskey(p0.decay_params,field[1])
       nmarks += 1
-      decay_params[field[1]] = [sigma_0,rho_0]
+      p0.decay_params[field[1]] = [sigma_0,rho_0]
     end
     push!(data, HawkesPoint(field[1],myparse(Float64,field[2])))
     println(data[i])
   end
-  println("\n$nmarks decay_params:\n",decay_params)
+  println("\n$nmarks decay_params:\n",p0.decay_params)
 
-  omega = Vector{Float64}(undef,ndata)
-  sigma = Vector{Float64}(undef,nmarks)
-  rho = Vector{Float64}(undef,nmarks)
-  lambda = lambda_0
-  
-  omega1 = Matrix{Float64}(undef,ndata,ndata) 
-  for i in 1:nmarks
-    sigma[i] = sigma_0
-    rho[i] = rho_0
-  end
+  omega1 = Matrix{Float64}(undef,ndata,ndata)
+#  k_hat = Matrix{Float64}(undef,ndata,ndata)
   rng = MersenneTwister(seed)
   sum = 0
   for i in 1:ndata
-    omega[i] = rand(rng)
-    sum += omega[i]
+    push!(p0.omega,rand(rng))
+    sum += p0.omega[i]
   end
-  for i in 1:ndata
-    omega[i] /= sum
-  end
-  println("omega: $omega")
+  p0.omega ./= sum
 
   #OK, here we go
-  p0 = Parameters(lambda,omega,rho,sigma)
+  k_hat = fill(0.0,(ndata,ndata))
   for i in 1:ndata
     t_i = data[i].time
     sum = 0
     for j in 1:i-1
-      sigma = decay_params[data[j].mark][1]
-      rho = decay_params[data[j].mark][2]
+      sigma = p0.decay_params[data[j].mark][1]
+      rho = p0.decay_params[data[j].mark][2]
       if j == 1
         t_j = 0
-        k_hat_ij = lambda*t_i
+        k_hat_0 = lambda*t_i
       else
         t_j = (j == 1 ? 0 : data[j].time)
-        k_hat_ij = p0.sigma*(exp(-p0.rho*t_j) - exp(-p0.rho*t_i))/p0.rho
+        k_hat_0 = p0.sigma*(exp(-p0.rho*t_j) - exp(-p0.rho*t_i))/p0.rho
       end
-      omega1[i,j] = p0.omega[j]*sqrt(k_hat_ij)/(t_i-t_j)
+      omega1[i,j] = p0.omega[j]*sqrt(k_hat_0)/(t_i-t_j)
       sum += omega1[i,j]
+      if i > j
+        k_hat[i,j] = omega1[i,j] + k_hat[i-1,j]
+      end
     end
     for j in 1:i-1
       omega1[i,j] /= sum
